c1e71b8d8a0f106af3c42a6874f4cfa6714ee578
Júlio Mansan <julio.mansan31@gmail.com>
2023-09-14 01:54:07 +0000

    Otimização do código em JavaScript

--- src/js/index.js
+++ src/js/index.js
@@ -1,104 +1,75 @@
-const mainPage = document.querySelector('.main-page')
-const aboutPage = document.querySelector('.about-page')
-const locationPage = document.querySelector('.location-page')
-const careersPage = document.querySelector('.careers-page')
+const mainPage = document.querySelector('.main-page');
+const aboutPage = document.querySelector('.about-page');
+const locationPage = document.querySelector('.location-page');
+const careersPage = document.querySelector('.careers-page');
 
-const aboutLabel = document.querySelectorAll('.about-label')
-const locationLabel = document.querySelectorAll('.location-label')
-const careersLabel = document.querySelectorAll('.careers-label')
+const aboutLabel = document.querySelectorAll('.about-label');
+const locationLabel = document.querySelectorAll('.location-label');
+const careersLabel = document.querySelectorAll('.careers-label');
 
-const aboutInput = document.querySelector('#about')
-const locationInput = document.querySelector('#location')
-const careersInput = document.querySelector('#careers')
+const aboutInput = document.querySelector('#about');
+const locationInput = document.querySelector('#location');
+const careersInput = document.querySelector('#careers');
 
-aboutInput.addEventListener('click', function (){
+const aboutFooter = document.querySelector('#about-footer');
+const locationFooter = document.querySelector('#location-footer');
+const careersFooter = document.querySelector('#careers-footer');
+
+function handlePageChange(selectedPage, selectedLabelColor, otherLabelColor) {
+    mainPage.classList.remove('selecionado');
+    aboutPage.classList.remove('selecionado');
+    locationPage.classList.remove('selecionado');
+    careersPage.classList.remove('selecionado');
+
+    selectedPage.classList.add('selecionado');
+
+    for (let i = 0; i < aboutLabel.length; i++) {
+        aboutLabel[i].style.color = otherLabelColor;
+        locationLabel[i].style.color = otherLabelColor;
+        careersLabel[i].style.color = otherLabelColor;
+    }
+
+    selectedLabelColor.forEach((label, index) => {
+        label.style.color = 'var(--color-yellow)';
+    });
+}
+
+aboutInput.addEventListener('click', function () {
     if (aboutInput.checked) {
-        mainPage.classList.remove('selecionado')
-        aboutPage.classList.add('selecionado')
-        locationPage.classList.remove('selecionado')
-        careersPage.classList.remove('selecionado')
-        for (let i = 0; i < aboutLabel.length; i++) {
-            aboutLabel[i].style.color = 'var(--color-yellow)'
-            locationLabel[i].style.color = 'var(--color-dim-grey)'
-            careersLabel[i].style.color = 'var(--color-dim-grey)'
-        }
+        handlePageChange(aboutPage, aboutLabel, 'var(--color-dim-grey)');
     }
-})
+});
 
-locationInput.addEventListener('click', function (){
+locationInput.addEventListener('click', function () {
     if (locationInput.checked) {
-        mainPage.classList.remove('selecionado')
-        aboutPage.classList.remove('selecionado')
-        locationPage.classList.add('selecionado')
-        careersPage.classList.remove('selecionado')
-        for (let i = 0; i < locationLabel.length; i++) {
-            aboutLabel[i].style.color = 'var(--color-dim-grey)'
-            locationLabel[i].style.color = 'var(--color-yellow)'
-            careersLabel[i].style.color = 'var(--color-dim-grey)'
-        }
+        handlePageChange(locationPage, locationLabel, 'var(--color-dim-grey)');
     }
-})
+});
 
-careersInput.addEventListener('click', function (){
+careersInput.addEventListener('click', function () {
     if (careersInput.checked) {
-        mainPage.classList.remove('selecionado')
-        aboutPage.classList.remove('selecionado')
-        locationPage.classList.remove('selecionado')
-        careersPage.classList.add('selecionado')
-        for (let i = 0; i < careersLabel.length; i++) {
-            aboutLabel[i].style.color = 'var(--color-dim-grey)'
-            locationLabel[i].style.color = 'var(--color-dim-grey)'
-            careersLabel[i].style.color = 'var(--color-yellow)'
-        }
+        handlePageChange(careersPage, careersLabel, 'var(--color-dim-grey)');
     }
-})
+});
 
-
-const aboutFooter = document.querySelector('#about-footer')
-const locationFooter = document.querySelector('#location-footer')
-const careersFooter = document.querySelector('#careers-footer')
-
-aboutFooter.addEventListener('click', function (){
+aboutFooter.addEventListener('click', function () {
     if (aboutFooter.checked) {
-        mainPage.classList.remove('selecionado')
-        aboutPage.classList.add('selecionado')
-        locationPage.classList.remove('selecionado')
-        careersPage.classList.remove('selecionado')
-        for (let i = 0; i < aboutLabel.length; i++) {
-            aboutLabel[i].style.color = 'var(--color-yellow)'
-            locationLabel[i].style.color = 'var(--color-dim-grey)'
-            careersLabel[i].style.color = 'var(--color-dim-grey)'
-        }
+        handlePageChange(aboutPage, aboutLabel, 'var(--color-dim-grey)');
     }
-})
+});
 
-locationFooter.addEventListener('click', function (){
+locationFooter.addEventListener('click', function () {
     if (locationFooter.checked) {
-        mainPage.classList.remove('selecionado')
-        aboutPage.classList.remove('selecionado')
-        locationPage.classList.add('selecionado')
-        careersPage.classList.remove('selecionado')
-        for (let i = 0; i < locationLabel.length; i++) {
-            aboutLabel[i].style.color = 'var(--color-dim-grey)'
-            locationLabel[i].style.color = 'var(--color-yellow)'
-            careersLabel[i].style.color = 'var(--color-dim-grey)'
-        }
+        handlePageChange(locationPage, locationLabel, 'var(--color-dim-grey)');
     }
-})
+});
 
-careersFooter.addEventListener('click', function (){
+careersFooter.addEventListener('click', function () {
     if (careersFooter.checked) {
-        mainPage.classList.remove('selecionado')
-        aboutPage.classList.remove('selecionado')
-        locationPage.classList.remove('selecionado')
-        careersPage.classList.add('selecionado')
-        for (let i = 0; i < careersLabel.length; i++) {
-            aboutLabel[i].style.color = 'var(--color-dim-grey)'
-            locationLabel[i].style.color = 'var(--color-dim-grey)'
-            careersLabel[i].style.color = 'var(--color-yellow)'
-        }
+        handlePageChange(careersPage, careersLabel, 'var(--color-dim-grey)');
     }
-})
+});
+
 
 const acc = document.querySelectorAll(".acordeon");
 var i;
